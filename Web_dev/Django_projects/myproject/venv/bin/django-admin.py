e88f245c54c074f22104d48db2786938e6890aa0
bennergarrett <bennergarrett@gmail.com>
2020-04-28 12:18:32 -0500

    removed old file

--- Web_dev/Django_projects/myproject/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/garrettbenner/Documents/Coding_Projects/Django_projects/myproject/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
